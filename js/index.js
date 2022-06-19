68f40508bfbe3f9fb7a2b615d624013dcd3ede20
Kingsley Agu <agukingsley96@gmail.com>
2022-06-19 18:14:57 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const cardRating = document.querySelector(".card-rating");
+const cardThank = document.querySelector(".card-thank");
+const cardSelect = document.querySelector(".card-thank__selection");
+const submitBtn = document.querySelector(".card-rating__btn");
+const rating = document.querySelectorAll(".card-rating__span");
+const span = document.querySelectorAll("span");
+
+rating.forEach((btn, i) => {
+  btn.addEventListener("click", ratingClick);
+  btn.addEventListener(
+    "click",
+    function select() {
+      let currentRating = i + 1;
+      cardSelect.innerText = `You selected ${currentRating} out of 5`;
+    },
+    true
+  );
+});
+
+function ratingClick(e) {
+  rating.forEach((btn, i) => {
+    btn.classList.remove("card-rating--active");
+  });
+  if (e.target.classList.contains("card-rating__span")) {
+    e.target.classList.add("card-rating--active");
+  }
+}
+
+submitBtn.addEventListener("click", thankYou);
+
+function thankYou() {
+  cardRating.classList.add("card--hidden");
+  cardThank.classList.remove("card--hidden");
+}
